1abca7347c2ebcedd117a29f8d0c5497fb30bcca
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-08-21 10:52:43 -0700

    add SearchBar component for location-based searches

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+type SearchProps = {
+  placeholder: string;
+  city: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function SearchBar({
+  placeholder,
+  city,
+  onChange,
+}: SearchProps) {
+  return (
+    <div className='p-6'>
+      <form className='flex gap-2 w-full max-w-md'>
+        <input
+          type='text'
+          value={city}
+          onChange={onChange}
+          placeholder={placeholder}
+          className='flex-1 px-4 py-2 border-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400'
+        />
+        <button
+          type='submit'
+          className='px-4 py-2 bg-blue-400 text-white rounded-xl hover:bg-blue-600'
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
