f1f423cb60a4360cb81b9792e56a0513f4a0ab96
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-08-21 21:15:28 -0700

    refactor loading, blank page if no data and add UVDisplay component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import SearchBar from './components/SearchBar';
 
 import { APIResponse } from './types';
+import UVDisplay from './components/UVDisplay';
 
 export default function HomePage() {
   const [city, setCity] = useState('');
@@ -49,24 +50,15 @@ export default function HomePage() {
         onSubmit={handleSearch}
       />
 
-      {loading && <p>Loading...</p>}
-      {error && <p className='text-red-500'>{error}</p>}
-
-      {data && (
-        <div className='mt-6 p-4 border rounded-lg'>
-          <div className='flex gap-2 content-center'>
-            <div className='text-xl font-semibold'>{data.city}</div>
-            <div className='font-semibold'>{data.weather.sys.country}</div>
-          </div>
-          <p className='text-4xl font-extrabold'>
-            UV Index: {data.uv?.result?.uv.toFixed(1)}
-          </p>
-          <p>Max UV: {data.uv.result.uv_max.toFixed(1)}</p>
-          <div>
-            {' '}
-            <p>Temperature: {data.weather?.main?.temp} °C</p>
-            <p>{data.weather.weather[0].description}</p>
-          </div>
+      {loading ? (
+        <div className='p-4 rounded-xl shadow-md bg-gray-200 text-gray-500 text-center'>
+          Loading...
+        </div>
+      ) : data ? (
+        <UVDisplay uv={data?.uv.result?.uv} uv_max={data?.uv.result.uv_max} />
+      ) : (
+        <div className='p-4 rounded-xl shadow-md bg-gray-200 text-gray-500 text-center'>
+          Search for a city
         </div>
       )}
     </main>
