13c13a34a813ebd8ceda9c436fc1b0ae3e4fa415
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-08-21 21:13:42 -0700

    add safety exposure time

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,6 +1,14 @@
 export interface UVData {
   uv: number;
   uv_max: number;
+  safe_exposure_time: {
+    st1: number;
+    st2: number;
+    st3: number;
+    st4: number;
+    st5: number;
+    st6: number;
+  };
   ozone: number;
 }
 
