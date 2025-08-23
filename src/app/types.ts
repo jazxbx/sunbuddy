eca9ae6e51dd19326bca0637cb2ca7bfd68ec633
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-08-22 22:27:57 -0700

    refactor UVData interface to use SafeExposureTime type and remove ozone property -add type SkinType

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,15 +1,7 @@
 export interface UVData {
   uv: number;
   uv_max: number;
-  safe_exposure_time: {
-    st1: number;
-    st2: number;
-    st3: number;
-    st4: number;
-    st5: number;
-    st6: number;
-  };
-  ozone: number;
+  safe_exposure_time: SafeExposureTime;
 }
 
 export interface WeatherData {
@@ -33,6 +25,17 @@ export interface WeatherData {
     speed: number;
     deg: number;
   };
+}
+
+export type SkinType = 1 | 2 | 3 | 4 | 5 | 6;
+
+export interface SafeExposureTime {
+  st1: number;
+  st2: number;
+  st3: number;
+  st4: number;
+  st5: number;
+  st6: number;
 }
 
 export interface APIResponse {
