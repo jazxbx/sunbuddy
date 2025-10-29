d4cefc34618298efd8a6d3c6598c3de8f4922318
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-10-28 17:36:23 -0700

    css change

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,5 +1,10 @@
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className='bg-white rounded-3xl p-5 border-gray-300'>{children}</div>
+    <div
+      className='bg-white rounded-3xl p-5 border-gray-300  min-w-[200px] 
+        min-h-[200px] '
+    >
+      {children}
+    </div>
   );
 }
