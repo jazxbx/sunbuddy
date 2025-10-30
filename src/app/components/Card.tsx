9729270776a313c5067a69a8781eac07b0da2996
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-10-30 16:33:34 -0700

    css changes

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,7 +1,7 @@
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className='    bg-white rounded-3xl p-5 border border-gray-100 
+      className=' bg-white rounded-3xl p-5 md:p-10 border border-gray-100 
         w-full h-full flex flex-col'
     >
       {children}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Playfair_Display, Lato } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
+import Footer from './components/Footer';
 
 const playfair_display = Playfair_Display({
   variable: '--font-playfair',
@@ -26,11 +27,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className='h-dvh'>
       <body
-        className={`${lato.variable} ${playfair_display.variable} bg-bg p-3 md:py-5 md:px-10 lg:px-48 bg-blue-50`}
+        className={`${lato.variable} ${playfair_display.variable} bg-bg py-3 px-4 md:py-5 md:px-10 lg:px-48 bg-blue-50 min-h-full flex flex-col`}
       >
         <Providers>{children}</Providers>
+        <Footer />
       </body>
     </html>
   );
